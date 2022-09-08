526d82a49c5161db17df3d08bbe61268bb5030eb
Georgi Todorov <111183006+19gtodorov14@users.noreply.github.com>
2022-09-08 21:43:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const api = {
+    key: "61abca79e89320af6407cdda876ca0da",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
